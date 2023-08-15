ddac3f02d5f547ab9b1893f4c4d5c12b8cc03cbd
Jordano <jgtomazoni@ucs.br>
2023-08-15 20:35:53 -0300

    WIP - Padrão Composite implementado;

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -4,15 +4,28 @@ namespace Alura\DesignPattern;
 use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
 use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
 
-class Orcamento{
-    public int $numeroDeItens;
-    public float $valor;
+class Orcamento implements Orcavel{
+    
+    private array $itens; 
+
     public EstadoOrcamento $estado;
 
     public function __construct() {
         $this->estado = new EmAprovacao();
+        $this->itens = [];
     }
 
+    public function valor(): float
+    {
+        return array_reduce(
+            $this->itens,
+            fn ($valorAcumulado, $item) => $item->valor() + $valorAcumulado, 
+        );
+    }
+
+    public function addItem(ItemOrcamento $item){
+        $this->itens[] = $item;
+    }
 
     public function aplicaDescontoExtra(){
         $this->valor -= $this->estado->calculaDescontoExtra($this);
